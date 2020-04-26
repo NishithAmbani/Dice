8a2b038d3dbe7c32fc381aec3eb2e69c8ae31559
Nishith Ambani <62053079+NishithAmbani@users.noreply.github.com>
2020-04-26 12:16:32 +0530

    Dice Game
    This is a basic 2 person Dice roll, higher number wins, Enjoy!!

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomnumber1=Math.floor(Math.random()*6)+1;
+document.querySelectorAll("img")[0].setAttribute("src","images\\dice"+randomnumber1+".png");
+var randomnumber2=Math.floor(Math.random()*6)+1;
+document.querySelectorAll("img")[1].setAttribute("src","images\\dice"+randomnumber2+".png");
+
+
+if(randomnumber1>randomnumber2)
+{
+  document.querySelector("h1").innerHTML="🚩Player 1 Wins";
+}
+else if (randomnumber1<randomnumber2)
+{
+  document.querySelector("h1").innerHTML="Player 2 Wins🚩";
+}
+else
+{
+  document.querySelector("h1").innerHTML="☮️It is a Draw☮️";
+}
